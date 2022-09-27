f0725292b29555448cb736a64d5a912c33e1ab50
James <knotsite@gmail.com>
2022-09-27 12:48:43 +0700

    feat: add store

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterSliceReducer from "../slices/counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterSliceReducer,
+  },
+});
