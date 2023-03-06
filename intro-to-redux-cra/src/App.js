e8f3bcec32621e84a3a5400115186723aa69d67d
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-03-06 19:52:38 +0700

    feat:complete counter app with redux w/ vite

---
+++ intro-to-redux-cra/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="counter-app">
+        <h1>Counter App</h1>
+        <h2>0</h2>
+        <div>
+          <button>+</button>
+          <button>-</button>
+          <button>Reset</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
